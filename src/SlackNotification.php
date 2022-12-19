0af9ebd7ac939f45ce6820c4799ef60ab1923dc8
Mathieu Henrotay <mathieu.henrotay@gmail.com>
2022-12-19 12:37:30 +0100

    🐛 Not extending laravel notification base class.

--- src/SlackNotification.php
+++ src/SlackNotification.php
@@ -3,8 +3,9 @@ namespace Deegitalbe\LaravelTrustupIoSlackNotifications;
 
 use Deegitalbe\LaravelTrustupIoSlackNotifications\Traits\Slack\IsSlackNotification;
 use Deegitalbe\LaravelTrustupIoSlackNotifications\Contracts\Slack\SlackNotificationContract;
+use Illuminate\Notifications\Notification;
 
-abstract class SlackNotification implements SlackNotificationContract
+abstract class SlackNotification extends Notification implements SlackNotificationContract
 {
     use IsSlackNotification;
 }
